ab5a50c92fa7d59cf5665776ad079e748e8eb8a1
mmorpgkrynn <44831277+mmorpgkrynn@users.noreply.github.com>
2018-11-07 10:03:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('message', message => {
  
      if (message.content === '!aiuto') {
 
-       message.channel.send('Questi sono i comandi disponibili:');
+       message.channel.send('Questi sono i comandi disponibili: \ !aiuto : questo messaggio \ !date : mostra le date associative importanti \ !tavoli : mostra i tavoli di gioco del semestre');
 
        }
  
