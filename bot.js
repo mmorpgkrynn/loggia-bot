6d35b432c0c927f9e9e1eb81651732e714b6955f
mmorpgkrynn <44831277+mmorpgkrynn@users.noreply.github.com>
2018-11-07 10:33:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,21 +22,16 @@ client.on('message', message => {
  
      if (message.content === '!aiuto') {
 
-       message.channel.send('Questi sono i comandi disponibili: \
-!aiuto : questo messaggio \
-!date : mostra le date associative importanti \
-!tavoli : mostra i tavoli di gioco del semestre');
+       message.channel.send('Questi sono i comandi disponibili: \n !aiuto : questo messaggio \n !link : collegamenti utili \n !date : mostra le date associative importanti \n !tavoli : mostra i tavoli di gioco del semestre');
 
        }
 
       if (message.content === '!date') {
-       message.channel.send('Prossimi appuntamenti: \
-');
+       message.channel.send('Prossimi appuntamenti: \n 30/11/2018: Cena Associativa \n 21/12/2018: Seconda Riunione associativa');
       }
  
       if (message.content === '!tavoli') {
-       message.channel.send('Tavoli di questo semestre: \
-');
+       message.channel.send('Tavoli di questo semestre: \n ');
       }
  
  
